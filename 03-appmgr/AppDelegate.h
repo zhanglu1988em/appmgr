19b46524a86a43502de7bd167d27abe47783b3ca
zhanglu1988em <zhanglu1988em163.com@zhanglu1988emdemacbook-pro.local>
2016-01-02 19:27:13 +0800

    Initial Commit

---
+++ 03-appmgr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-appmgr
+//
+//  Created by zhanglu1988em on 16/1/2.
+//  Copyright © 2016年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
